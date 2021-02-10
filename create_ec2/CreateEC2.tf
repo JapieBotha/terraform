d16fc6a5ddf8036590c955e3b80a0b2ccccee794
Japie Botha <japie.botha@eoh.co.za>
2021-02-10 12:33:11 +0200

    commit

--- create_ec2/CreateEC2.tf
+++ create_ec2/CreateEC2.tf
@@ -29,6 +29,14 @@ resource "aws_security_group" "ubuntu" {
   }
 
   ingress {
+    description = "FTP"
+    from_port   = 21
+    to_port     = 21
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
